325facb7f7457be1d71d3fd96fa8bdf1e8bb60ee
Carlos Vinícyus <carlosvinicyuss@gmail.com>
2025-04-07 10:49:11 -0300

    primeira-versao-completa

--- src/Contador.java
+++ src/Contador.java
@@ -1,6 +1,43 @@
+import java.util.Scanner;
+
 public class Contador {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParametrosInvalidosException {
 
+        Scanner terminal = new Scanner(System.in);
+
+        int[] parametros = lerParametros(terminal);
+
+        try {
+            contar(parametros[0], parametros[1]);
+        }
+        catch (ParametrosInvalidosException e) {
+            e.getMessage();
+            parametros = lerParametros(terminal);
+            contar(parametros[0], parametros[1]);
+        }
+        finally {
+            terminal.close();
+        }
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        if (parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException("O primeiro parâmetro não pode ser MAIOR que o segundo.");
+        }
+
+        int contagem = parametroDois - parametroUm;
+        for (int i = 1; i <= contagem; i++) {
+            System.out.println("Imprimindo o número " + i);
+        }
+    }
+
+    static int[] lerParametros(Scanner terminal) {
+        System.out.print("Digite o primeiro parâmetro: ");
+        int parametroUm = terminal.nextInt();
+        System.out.print("Digite o segundo parâmetro: ");
+        int parametroDois = terminal.nextInt();
+        int[] parametros = {parametroUm, parametroDois};
+        return parametros;
     }
 }
